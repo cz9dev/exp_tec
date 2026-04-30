63fae3808862d97c5111ffd19c7dca4a4043979c
cz9dev <cz9dev@gmail.com>
2026-04-30 16:25:50 -0400

    [BUG] En Partes y piezas, cuando los componentes se han desactivado aun aparecen Fixes #128

--- models/deviceModel.js
+++ models/deviceModel.js
@@ -127,14 +127,14 @@ class DeviceModel {
 
   static async getAvailableComponents() {
     const [rows] = await pool.execute(
-      "SELECT c.*, tc.nombre as tipo_componente FROM componente c, tipo_componente tc WHERE c.id_tipo_componente = tc.id AND c.id NOT IN (SELECT id_componente FROM dispositivo_componente)",
+      "SELECT c.*, tc.nombre as tipo_componente FROM componente c, tipo_componente tc WHERE c.id_tipo_componente = tc.id AND c.id NOT IN (SELECT id_componente FROM dispositivo_componente) AND deactivated_at IS NULL",
     );
     return rows;
   }
 
   static async getAvailablePeripherals() {
     const [rows] = await pool.execute(
-      "SELECT p.*, tp.nombre as tipo_periferico FROM periferico p, tipo_periferico tp WHERE p.id_tipo_periferico = tp.id AND p.id NOT IN (SELECT id_periferico FROM dispositivo_periferico)",
+      "SELECT p.*, tp.nombre as tipo_periferico FROM periferico p, tipo_periferico tp WHERE p.id_tipo_periferico = tp.id AND p.id NOT IN (SELECT id_periferico FROM dispositivo_periferico) AND deactivated_at IS NULL",
     );
     return rows;
   }
